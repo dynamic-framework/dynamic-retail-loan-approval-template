ffbc08263fb9faf4f7e1a5bf2a87227649a8e4b2
lnazarit <lnazarit@gmail.com>
2025-01-09 16:02:58 -0300

    refactor: bff

--- src/services/mappers/accountMapper.ts
+++ src/services/mappers/accountMapper.ts
@@ -1,23 +1,11 @@
 import type { ApiAccount } from '../api-interface';
-import {
-  AccountBaseType,
-  ApiAccountTypeConfig,
-} from '../config';
 import type { DepositAccount } from '../interface';
 
 export default function accountMapper(apiAccount: ApiAccount): DepositAccount {
-  const commonProps = {
-    id: apiAccount.id,
-    name: apiAccount.nickName,
-    alias: apiAccount.nickName,
-    accountNumber: apiAccount.accountNumber,
-    type: ApiAccountTypeConfig[apiAccount.accountType],
-  };
-
   return {
-    ...commonProps,
-    baseType: AccountBaseType.Deposit,
-    accountingBalance: apiAccount.accountingBalance,
-    balanceAvailable: apiAccount.depositDetails?.balances.available as number,
+    id: apiAccount.id,
+    number: apiAccount.number,
+    maskedNumber: apiAccount.masked_number,
+    accountHolderName: apiAccount.account_holder_name,
   };
 }
